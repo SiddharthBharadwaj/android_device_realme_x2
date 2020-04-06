4484abf0033c4d791064a65c2ae37788855d8c5e
Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
2020-04-06 11:56:59 +0000

    x2: Initialize Havoc

--- havoc_x2.mk
+++ havoc_x2.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/realme/x2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_x2
+PRODUCT_NAME := havoc_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
