626857ba367d7517644425d39025970acb45a569
slaughtermc <47281879+slaughtermc@users.noreply.github.com>
2020-04-30 01:18:41 +0530

    Update and rename havoc_x2.mk to Arrow_x2.mk

--- Arrow_x2.mk
+++ Arrow_x2.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/realme/x2/device.mk)
 
 # Inherit some common Havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_x2
+PRODUCT_NAME := arrow_x2
 PRODUCT_DEVICE := x2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme x2
